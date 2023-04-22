663345775fabf2b82912c22057f7ee797d687661
NikolayYakov <niki012018@abv.bg>
2023-04-22 18:31:20 +0300

    added service and added a layer of abstraction

---
+++ Services/IReservingTicketsService.cs
@@ -0,0 +1,9 @@
+﻿using SamoLetoAPI.DTO;
+
+namespace SamoLetoAPI.Services
+{
+    public interface IReservingTicketsService
+    {
+        public Task<BaseResponseDTO> ReserveSeat(string flightnumber);
+    }
+}
--- Services/ReservingTicketsService.cs
+++ Services/ReservingTicketsService.cs
@@ -1,6 +1,31 @@
-﻿namespace SamoLetoAPI.Services
+﻿using SamoLetoAPI.Controllers;
+using SamoLetoAPI.DTO;
+using SamoLetoAPI.Singleton;
+
+namespace SamoLetoAPI.Services
 {
-    public class ReservingTicketsService
+    public class ReservingTicketsService : IReservingTicketsService
     {
+        private readonly ISharedDictionary _sharedDictionary;
+        private ILogger<ReservingTicketsService> _logger;
+
+        public ReservingTicketsService(ILogger<ReservingTicketsService> logger, ISharedDictionary sharedDictionary)
+        {
+            _logger = logger;
+            _sharedDictionary = sharedDictionary;
+        }
+
+        public async Task<BaseResponseDTO> ReserveSeat(string flightnumber)
+        {
+            try
+            {
+                return await _sharedDictionary.ReserveSeat(flightnumber);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return new(ErrorCode.DefaultError);
+            }
+        } 
     }
 }
